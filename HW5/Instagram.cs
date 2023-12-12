fb5d772d857b632ae24c1aa665b6b818773aabf4
ant-guz <anton.guz87@gmail.com>
2023-12-12 15:12:15 +0200

    Done homework 5

---
+++ HW5/Instagram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    public class Instagram : AbstractSocialNetwork
+    {
+        public Instagram(string login, string password) : base(login, password)
+        {
+        }
+
+        public override void CreatePost(string message)
+        {
+            bool loginSuccessfully = LogIn(Login, Password);
+            if (loginSuccessfully)
+            {
+                Console.WriteLine($"----->Add picture.");
+                Publish(message);
+                LogOut();
+            }
+        }
+        public override bool LogIn(string login, string password)
+        {
+            Console.WriteLine($"Successfully login into Instagram");
+            return true;
+        }
+
+        public override void LogOut()
+        {
+            Console.WriteLine($"Successfully logout from Instagram");
+        }
+
+        public override void Publish(string message)
+        {
+            Console.WriteLine($"Successfully publish a Instagram message: {message}");
+        }
+    }
+}
---
+++ HW5/X.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    public class X : AbstractSocialNetwork
+    {
+        public X(string login, string password) : base(login, password)
+        {
+        }
+
+        public override bool LogIn(string login, string password)
+        {
+            Console.WriteLine($"Successfully login into X");
+            return true;
+        }
+
+        public override void LogOut()
+        {
+            Console.WriteLine($"Successfully logout from X");
+        }
+
+        public override void Publish(string message)
+        {
+            Console.WriteLine($"Successfully publish a X message: {message}");
+        }
+    }
+}
